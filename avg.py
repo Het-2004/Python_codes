81275181ec60e4b8202604554432e85adbf43380
HET SOLANKI <solankihetpravinbhai2122004@gmail.com>
2024-03-27 19:23:09 +0530

    average of 3 numbers

---
+++ avg.py
@@ -0,0 +1,7 @@
+a = input("Enter 1st number : ")
+b = input("Enter 2nd number : ")
+c = input("Enter 3rd number : ")
+
+avg = (float(a) + float(b) + float(c)) / 3
+
+print("Average of three number =",avg)
