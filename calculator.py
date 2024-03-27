1f10e583894c616012a58b70302be7f7b7695dba
HET SOLANKI <solankihetpravinbhai2122004@gmail.com>
2024-03-27 19:24:34 +0530

    Simple calculator

---
+++ calculator.py
@@ -0,0 +1,12 @@
+a = input("Enter 1st number: ")
+b = input("Enter 2nd number: ")
+
+sum = float(a) + float(b)
+diff = float(a) - float(b)
+multi = float(a) * float(b)
+div = float(a) / float(b)
+
+print("The sum is: ", sum)
+print("The Differentiate is: ", diff)
+print("The Multiplication is: ", multi)
+print("The division is: ", div)
